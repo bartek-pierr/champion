f915fc54b0cc816bd4ac23932229e416f895d3ad
JakubOlkowicz <olkuba@poczta.fm>
2024-01-21 20:49:39 +0100

    finish front

---
+++ public/data/offers.js
@@ -0,0 +1,80 @@
+const dataOffers = {
+  "1": {
+    "title": "Offers 1",
+    "photo": "public/images/offers/offers1.png",
+	  "tags": "normal",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent nec nulla elementum sapien commodo pretium. Sed tincidunt suscipit rhoncus. Nunc ac mauris vitae ligula imperdiet dignissim. Fusce in diam ultricies, maximus nisi in, imperdiet velit. Cras convallis eros nunc, id pulvinar velit luctus sed. Mauris rutrum tortor at porttitor cursus. Mauris lacus odio, efficitur sed fermentum sit amet, sagittis in nunc. "
+  },
+  "2": {
+    "title": "Offers 2",
+    "photo": "public/images/offers/offers2.png",
+	  "tags": "normal",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent nec nulla elementum sapien commodo pretium. Sed tincidunt suscipit rhoncus. Nunc ac mauris vitae ligula imperdiet dignissim. Fusce in diam ultricies, maximus nisi in, imperdiet velit. Cras convallis eros nunc, id pulvinar velit luctus sed. Mauris rutrum tortor at porttitor cursus. Mauris lacus odio, efficitur sed fermentum sit amet, sagittis in nunc. "
+  },
+  "3": {
+    "title": "Offers 3",
+    "photo": "public/images/offers/offers3.png",
+	  "tags": "normal",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent nec nulla elementum sapien commodo pretium. Sed tincidunt suscipit rhoncus. Nunc ac mauris vitae ligula imperdiet dignissim. Fusce in diam ultricies, maximus nisi in, imperdiet velit. Cras convallis eros nunc, id pulvinar velit luctus sed. Mauris rutrum tortor at porttitor cursus. Mauris lacus odio, efficitur sed fermentum sit amet, sagittis in nunc. "
+  },
+  "4": {
+    "title": "Offers 4",
+    "photo": "public/images/offers/offers4.png",
+	  "tags": "normal",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent nec nulla elementum sapien commodo pretium. Sed tincidunt suscipit rhoncus. Nunc ac mauris vitae ligula imperdiet dignissim. Fusce in diam ultricies, maximus nisi in, imperdiet velit. Cras convallis eros nunc, id pulvinar velit luctus sed. Mauris rutrum tortor at porttitor cursus. Mauris lacus odio, efficitur sed fermentum sit amet, sagittis in nunc. "
+  },
+  "5": {
+    "title": "Offers 5",
+    "photo": "public/images/offers/offers5.png",
+	  "tags": "normal",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent nec nulla elementum sapien commodo pretium. Sed tincidunt suscipit rhoncus. Nunc ac mauris vitae ligula imperdiet dignissim. Fusce in diam ultricies, maximus nisi in, imperdiet velit. Cras convallis eros nunc, id pulvinar velit luctus sed. Mauris rutrum tortor at porttitor cursus. Mauris lacus odio, efficitur sed fermentum sit amet, sagittis in nunc. "
+  },
+  "6": {
+    "title": "Offers 6",
+    "photo": "public/images/offers/offers6.png",
+	  "tags": "normal",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent nec nulla elementum sapien commodo pretium. Sed tincidunt suscipit rhoncus. Nunc ac mauris vitae ligula imperdiet dignissim. Fusce in diam ultricies, maximus nisi in, imperdiet velit. Cras convallis eros nunc, id pulvinar velit luctus sed. Mauris rutrum tortor at porttitor cursus. Mauris lacus odio, efficitur sed fermentum sit amet, sagittis in nunc. "
+  },
+  "front_1": {
+    "key": "front_1",
+    "title": "Front 1",
+    "photo": "public/images/offers/offers1.png",
+	  "tags": "front",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent nec nulla elementum sapien commodo pretium. Sed tincidunt suscipit rhoncus. Nunc ac mauris vitae ligula imperdiet dignissim. Fusce in diam ultricies, maximus nisi in, imperdiet velit. Cras convallis eros nunc, id pulvinar velit luctus sed. Mauris rutrum tortor at porttitor cursus. Mauris lacus odio, efficitur sed fermentum sit amet, sagittis in nunc. "
+  },
+  "front_2": {
+    "key": "front_2",
+    "title": "Front 2",
+    "photo": "public/images/offers/offers2.png",
+	  "tags": "front",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent nec nulla elementum sapien commodo pretium. Sed tincidunt suscipit rhoncus. Nunc ac mauris vitae ligula imperdiet dignissim. Fusce in diam ultricies, maximus nisi in, imperdiet velit. Cras convallis eros nunc, id pulvinar velit luctus sed. Mauris rutrum tortor at porttitor cursus. Mauris lacus odio, efficitur sed fermentum sit amet, sagittis in nunc. "
+  },
+  "front_3": {
+    "key": "front_3",
+    "title": "Front 3",
+    "photo": "public/images/offers/offers3.png",
+	  "tags": "front",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent nec nulla elementum sapien commodo pretium. Sed tincidunt suscipit rhoncus. Nunc ac mauris vitae ligula imperdiet dignissim. Fusce in diam ultricies, maximus nisi in, imperdiet velit. Cras convallis eros nunc, id pulvinar velit luctus sed. Mauris rutrum tortor at porttitor cursus. Mauris lacus odio, efficitur sed fermentum sit amet, sagittis in nunc. "
+  },
+  "front_4": {
+    "key": "front_4",
+    "title": "Front 4",
+    "photo": "public/images/offers/offers4.png",
+	  "tags": "front",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent nec nulla elementum sapien commodo pretium. Sed tincidunt suscipit rhoncus. Nunc ac mauris vitae ligula imperdiet dignissim. Fusce in diam ultricies, maximus nisi in, imperdiet velit. Cras convallis eros nunc, id pulvinar velit luctus sed. Mauris rutrum tortor at porttitor cursus. Mauris lacus odio, efficitur sed fermentum sit amet, sagittis in nunc. "
+  },
+  "front_5": {
+    "key": "front_5",
+    "title": "Front 5",
+    "photo": "public/images/offers/offers5.png",
+	  "tags": "front",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent nec nulla elementum sapien commodo pretium. Sed tincidunt suscipit rhoncus. Nunc ac mauris vitae ligula imperdiet dignissim. Fusce in diam ultricies, maximus nisi in, imperdiet velit. Cras convallis eros nunc, id pulvinar velit luctus sed. Mauris rutrum tortor at porttitor cursus. Mauris lacus odio, efficitur sed fermentum sit amet, sagittis in nunc. "
+  },
+  "front_6": {
+    "key": "front_6",
+    "title": "Front 6",
+    "photo": "public/images/offers/offers6.png",
+	  "tags": "front",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent nec nulla elementum sapien commodo pretium. Sed tincidunt suscipit rhoncus. Nunc ac mauris vitae ligula imperdiet dignissim. Fusce in diam ultricies, maximus nisi in, imperdiet velit. Cras convallis eros nunc, id pulvinar velit luctus sed. Mauris rutrum tortor at porttitor cursus. Mauris lacus odio, efficitur sed fermentum sit amet, sagittis in nunc. "
+  }
+}
--- readme.md
+++
@@ -1,1 +0,0 @@
-https://github.com/bartek-pierr/champion
